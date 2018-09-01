de4df0967cb67027794dcc099d6aeda96456f269
sbhatore95 <sbhatore95@gmail.com>
2018-09-01 19:58:56 +0530

    tfidf and structure changed

---
+++ isec_2019/isec/scrape/tfidf_pdf/final.py
@@ -0,0 +1,16 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+corpus = []
+for i in range (1, 6):
+	file = open("file"+str(i)+".txt", "r")
+	corpus.append(file.read())
+vectorizer = TfidfVectorizer(min_df=1)
+X = vectorizer.fit_transform(corpus)
+idf = vectorizer.idf_
+dic = dict(zip(vectorizer.get_feature_names(), idf))
+s = [(k, dic[k]) for k in sorted(dic, key=dic.get)]
+file = open("ans.txt", "w")
+file2 = open("ans2.txt", "w")
+for key, value in s:
+	file.write(str(value)+'\n')
+	file2.write(key+'\n')
+# print(s)
---
+++ isec_2019/isec/scrape/tfidf_pdf/tfidf.py
@@ -0,0 +1,57 @@
+# 27.08.18
+# Code for TFIDF
+import enchant
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import wordnet
+import codecs
+import re
+# For checking if word is english
+d = enchant.Dict("en_US")
+
+# maximum stop words
+stop_words = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+def hasNumber(str):
+	for i in str:
+		if i.isdigit():
+			return True
+	return False
+
+# txt files are there around 78 of them.
+arr = ['bhc.txt', 'IPOL_BRI(2018)614491_EN.txt', 'Reserve Bank of India - Master Circulars.txt', 'npl_guidance.en.txt', 'non-performing-loans-regulatory-and-accounting-treatments-of-assets.txt']
+for i in arr:
+	with codecs.open(i, "r",encoding='utf-8', errors='ignore') as file:
+		r = file.read()
+	wordList = re.sub("[^\w]", " ",  r).split()
+	file = open("file"+str(i)+".txt", "w")
+	
+	# This is for lemmatizing
+	lemmatiser = WordNetLemmatizer()
+	a = ''
+	for k in wordList:
+		a = a + k + ' '
+	tokens = word_tokenize(a)
+	tokens_pos = pos_tag(tokens)
+	dic = {}
+	for i in tokens_pos:
+		dic[i[0]] = i[1]
+
+	for j in wordList:
+		if j not in stop_words and d.check(j) and len(j)>3 and j!='' and not dic[j].startswith('J') and not dic[j].startswith('R') and not hasNumber(j):
+			file.write(lemmatiser.lemmatize(j.lower(), get_wordnet_pos(dic[j])))
+			file.write('\n')
